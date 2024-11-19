593861c0deb41b8f68cae3b6236be50982930ed0
Shehan Vinod <110800392+ShehanGit@users.noreply.github.com>
2024-11-19 13:16:58 +0530

    error

--- backend/src/main/java/com/weather_forecasting_system/config/WebConfig.java
+++ backend/src/main/java/com/weather_forecasting_system/config/WebConfig.java
@@ -1,14 +1,29 @@
 package com.weather_forecasting_system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${images.path}")
+    private String imagesPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///C:/Projects/Agricultural-weather-forecasting-System/backend/src/main/resources/images/");
+                .addResourceLocations("file:///" + imagesPath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/images/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
